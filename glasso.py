75df64c84263f4985d27a8b3f32abce2c6d70c59
Mo Shi <mo.shi@rutgers.edu>
2019-05-11 15:55:45 -0400

    bug fixed
    now able to deal with original data set

--- glasso.py
+++ glasso.py
@@ -3,21 +3,23 @@ import pandas as pd
 from sklearn.linear_model import lars_path
 from generator import data_generator
 import scipy.linalg as la
+from scipy import stats
 
 
-def Glasso(A, lambda_=0.005, iter=100):
+def Glasso(A, lambda_=0.001, iter=500):
 	# row: features, column: samples
 	m = A.shape[1]
 	k = A.shape[0]
-	S = np.cov(A)
+	#S = np.cov(A)
+	S = np.corrcoef(A)
 	# W: covariance matrix, T: precision matrix (W's inverse)
-	W = S.copy() + 0.001 * np.eye(k) # how to choose the lambda value?
+	W = S.copy() + 0.0001 * np.eye(k) # how to choose the lambda value?
 	T = np.linalg.inv(W)
 	# take the absolute of all negative values in the covariance matrix
-	'''
+	
 	print('Before:')
 	print(W)
-	'''
+	
 	indices = np.arange(k)
 	for n in range(iter):
 		for i in range(k):
@@ -26,10 +28,11 @@ def Glasso(A, lambda_=0.005, iter=100):
 			S_12 = W[indices != i, i]
 			#solve the lasso problem
 			
-			_, _, coefs_ = lars_path( W_11, S_12, Xy = S_12, Gram = W_11,
-										alpha_min = lambda_/(k-1), copy_Gram = True,
-										method = "lasso")
+			_, _, coefs_ = lars_path( W_11, S_12, Xy = S_12, Gram = W_11, copy_Gram = True, method = "lar",
+									alpha_min = lambda_/(k-1), max_iter = 10)
+			# alpha_min = lambda_/(k-1)
 			coefs_ = coefs_[:,-1] # only the last column
+			# print(coefs_)
 			'''
 			X = la.sqrtm(W_11)
 			y = np.dot(np.linalg.inv(X), S_12)
@@ -43,10 +46,10 @@ def Glasso(A, lambda_=0.005, iter=100):
 			temp_coefs = np.dot(W_11, coefs_)
 			W[i, indices != i] = temp_coefs
 			W[indices != i, i] = temp_coefs
-		'''
+		
 		print('Iter:', n+1)
 		print(W)
-		'''
+		
 		# test convergence
 		if np.abs(dual_gap(W, T, lambda_)) < 0.1: # should S be W here?
 			break
@@ -54,14 +57,16 @@ def Glasso(A, lambda_=0.005, iter=100):
 	print('After:')
 	print(W)
 	'''
+
 	graph = build_graph(W)
 	return graph
 
 
-def lasso_regression(X, y, weights, lambda_, iter=300):
+def lasso_regression(X, y, weights, lambda_, iter=50):
 	# k = number of coordinates
 	k = len(weights)
 	for n in range(iter):
+		print(n)
 		for i in range(k):
 			ceiling = -np.dot(X[:, i], y-np.dot(X, weights))
 			floor = np.dot(X[:, i], X[:, i])
@@ -81,25 +86,7 @@ def dual_gap(emp_cov, precision_, alpha):
     gap = np.sum(emp_cov * precision_)
     gap -= precision_.shape[0]
     gap += alpha * (np.abs(precision_).sum() - np.abs(np.diag(precision_)).sum())
-    return gap 
-
-
-def find_negative_values(matrix):
-	rows = matrix.shape[0]
-	columns = matrix.shape[1]
-	negative_positions = []
-	for i in range(rows):
-		for j in range(columns):
-			if matrix[i, j] < 0:
-				matrix[i, j] = -matrix[i, j]
-				negative_positions.append((i, j))
-	return matrix, negative_positions
-
-
-def recover_negative_values(matrix, negative_positions):
-	for pos in negative_positions:
-		matrix[pos[0], pos[1]] = -matrix[pos[0], pos[1]]
-	return matrix
+    return gap
 
 
 def build_graph(matrix):
@@ -123,13 +110,22 @@ def read_data(name):
 
 if __name__ == '__main__':
 	# read the data set
-	data = read_data('example.csv')
+	data = read_data('data_set.csv')
 	names = list(data.columns)
 	data = data.values.T
+	row = data.shape[0]
+	column = data.shape[1]
+	for i in range(row):
+		mode = stats.mode(data[i, :])[0][0]
+		for j in range(column):
+			if np.isnan(data[i, j]):
+				data[i, j] = mode
+	#result = pd.DataFrame(data)
+	#result.to_csv('new_data.csv', index=False)
+	
 	# using graphical lasso to learn the graph
 	graph = Glasso(data)
+	
 	# save the graph to a csv file
 	result = pd.DataFrame(graph, columns=names)
 	result.to_csv('graph.csv', index=False)
-
-	
